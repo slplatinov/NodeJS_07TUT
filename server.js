821ef06b2efc5b89925ab92f9ce1f43941e18490
SergeyPlatinov <slplatinov@yandex.ru>
2025-01-08 22:18:42 +0300

    server.js use urlencoded and serve static files

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3500;
 
+//built-in middleware to handle urlencoded data
+//in other worlds, form data:
+//content-type: application/x-www-form-urlencoded
+//express.urlencoded([options])
+app.use(express.urlencoded({encoded:false}));
+
+//build-in middleware for json
+app.use(express.json());
+
+//serve static files
+// app.use(express.static(path.join(__dirname,'/public')));
+
 app.get('^/$|/index(.html)?', (req, res) => {
     //res.sendFile('./views/index.html', { root: __dirname });
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
